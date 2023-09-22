83afe90f3580264e83beb28bd47aa66a0be2e4c6
Diego Silva <diegohss@live.com>
2023-09-23 09:13:56 +1200

    New changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 import Dashboard from './Components/Dashboard';
 import Navbar from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <h1>Tickets Dashboard</h1>
+        <h1>Helpdesk Tickets</h1>
       </header>
 
       <main className='body'>
-        <Dashboard />
+      <Dashboard/>
       </main>
 
     </div>
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,10 +1,48 @@
-function Dashboard() {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+
+function CardCreate() {
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Conteúdo do Dashboard</p>
+        <Card style={{ width: '42rem' }}>
+            <Card.Header>
+            <Nav variant="pills" defaultActiveKey="#first" >
+                    <Nav.Item>
+                        <Nav.Link href="#first">New</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#open">Open</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#unassigned">Unassigned</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#assignedme">Assigned to me</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#all">All tickets</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#disabled" disabled>
+                            Disabled
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>New tickets</Card.Title>
+                <Card.Text>
+                    Here are the content of new tickets
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
         </div>
     );
 }
 
-export default Dashboard;
+export default CardCreate;
